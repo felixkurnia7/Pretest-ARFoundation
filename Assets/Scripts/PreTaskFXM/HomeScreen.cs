7dbc127e110f45283ee77234c888bad36e88609c
felixkurnia7 <72884846+felixkurnia7@users.noreply.github.com>
2025-10-02 21:26:20 +0700

    Add basic Home Screen

---
+++ Assets/Scripts/PreTaskFXM/HomeScreen.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeScreen : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitApplication();
+        }
+    }
+
+    public void StartApp()
+    {
+        SceneManager.LoadScene("ARScene");
+    }
+
+    public void QuitApplication()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
